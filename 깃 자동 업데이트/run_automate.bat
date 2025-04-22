df02fb2747c42514a4a087876857445fb3768c5f
gibupanda <facecom2000@gmail.com>
2025-04-23 03:39:17 +0900

    배치파일 수정

--- 깃 자동 업데이트/run_automate.bat
+++ 깃 자동 업데이트/run_automate.bat
@@ -1,3 +1,7 @@
 @echo off
 echo 자동 동기화를 시작합니다...
-python "파이썬 경로를 입력해주세요"
+start cmd /k python "파이썬 경로를 입력하세요요"
+echo Python 스크립트가 새 창에서 실행되었습니다.
+echo 이 창은 3초 후 자동으로 닫힙니다.
+timeout /t 3 /nobreak > nul
+exit
